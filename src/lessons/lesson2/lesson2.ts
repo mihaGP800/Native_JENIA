68362b6c75e19851e339b35ea1ea0b287b23786a
mihaGP800 <mihagoroh@gmail.com>
2022-04-15 17:36:41 +0300

    lesson3

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -24,16 +24,51 @@ console.log('lesson 2');
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
 
-function sum(a: number) {
+// const sum = (a:number)=>(b:number)=>a+b
+
+function sum(a = 100) {
     return function (b: number) {
         return a + b
     }
 }
 
-// console.log(sum(3)(6));
+const sumWith100 = sum(100)
 
-const sum1 = (a: number) => (b: number) => a + b
-// console.log(sum1(3)(6));
+// console.log(sumWith100(6))
+
+
+function urlDomain(domain: string) {
+    return function (url: string) {
+        return `https://${url}.${domain}`
+    }
+}
+
+const comUrl = urlDomain('com')
+const ruUrl = urlDomain('ru')
+// console.log(comUrl('google'))
+// console.log(ruUrl('google'))
+
+function logPerson() {
+
+    // @ts-ignore
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+
+const person1 = {name: 'Miha', age: 32, job: 'developer'}
+const person2 = {name: 'Alina', age: 27, job: 'project-manager'}
+
+type FnType = typeof logPerson
+type PersonType = typeof person1
+
+function bind(context: PersonType, fn: FnType) {
+    return function (...arg: any) {
+        fn.apply(context, arg)
+    }
+}
+
+// bind(person1, logPerson)()
+// bind(person2, logPerson)()
+
 
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
@@ -70,7 +105,21 @@ const sum1 = (a: number) => (b: number) => a + b
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
 
-
+// function makeCounter() {
+//     // let count = 0
+//     return function (count: number) {
+//         return {
+//             increase: ++count,
+//             decrease: --count,
+//             reset: 0,
+//             set: count
+//         }
+//     }
+// }
+//
+// const counter = makeCounter();
+// console.log(counter(3).increase)
+// console.log(counter(40).reset)
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -82,10 +131,97 @@ const sum1 = (a: number) => (b: number) => a + b
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
+const superSum = (n: number) => {
+    if (n <= 0) return 0
+    if (n === 1) return (arg: number) => arg
+
+    let arr: number[] = []
+
+    const inner = (...arg: number[]) => {
+        arr = [...arr, ...arg]
+        if (arr.length >= n) {
+            return arr.splice(0, 3).reduce((acc, el) => acc + el)
+        } else return inner
+    }
+    return inner
+}
+
+// console.log(superSum(0));
+// //@ts-ignore
+// console.log(superSum(3)(2)(5)(3));
+// //@ts-ignore
+// console.log(superSum(2)(2)(5));
+// //@ts-ignore
+// console.log(superSum(3)(2)(5, 3));
+// //@ts-ignore
+// console.log(superSum(3)(2, 5, 3));
+// //@ts-ignore
+// console.log(superSum(3)(2, 5)(3));
+// //@ts-ignore
+// console.log(superSum(3)(2, 5)(3, 9));
+
+
 // P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
 
 // Task 05
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
+
+// const sumTo = (n: number) => {
+//     let res = 0
+//     for (let i = 1; i <= n; i++) {
+//         res += i
+//     }
+//     return res
+//     return [...Array(n)].map((_,i)=>i + 1).reduce((acc,el)=>acc+el)
+// }
+
+// const sumTo = (n: number): number => {
+//         // if (n!== 0){
+//         //     return n + sumTo(n-1)
+//         // }else return 0
+//     return n!== 0 ? n + sumTo(n-1) : 0
+// }
+
+
+// const multiTo = (n: number): number => {
+//     return n !== 1 ? n * multiTo(n - 1) : 1
+// }
+
+// const fib = (n:number):number => {
+//   return n <= 1 ? n : fib(n-1) + fib(n-2)
+// }
+
+// let list = {value: 1, next: {value: 2, next: {value: 3, next: {value: 4, next: null}}}};
+//
+// function printList(list: any) {
+//     ; // выводим текущий элемент
+//
+//     if (list.next) {
+//         console.log(list.next)
+//         printList(list.next); // делаем то же самое для остальной части списка
+//     }
+//     alert(list.value)
+// }
+
+// const flatToBase = (n: any[]) => {
+//     let arr: any[] = []
+//     for (let i = 0; i < n.length; i++) {
+//         if (Array.isArray(n[i])) {
+//             let newArr = flatToBase(n[i])
+//             for (let j = 0; j < newArr.length; j++) {
+//                 arr.push(newArr[j])
+//             }
+//         } else {
+//             arr.push(n[i])
+//         }
+//     }
+//     return arr
+// }
+
+const flatToBase = (nArr: any[]): any[] => nArr.reduce((acc, el) => acc.concat(Array.isArray(el) ? flatToBase(el): el), [])
+
+// console.log(flatToBase([[[[[[[42]]]], 36]]]))
+
 
 // Task 06
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,6 @@
+import {log} from 'util';
+import axios from 'axios';
+
 console.log('lesson 3');
 
 // Event loop
@@ -11,10 +14,92 @@ console.log('lesson 3');
 // https://learn.javascript.ru/promise-basics
 // https://www.youtube.com/watch?v=1idOY3C1gYU
 
+// setTimeout(() => console.log(1), 0)
+// setTimeout(() => console.log(2), 0)
+//
+// const pr = new Promise((res, rej) => {
+//     console.log(2.1)
+//     // res()
+//     setTimeout(() => rej(), 1000)
+//     console.log(2.2)
+// })
+// console.log(pr)
+//
+// pr
+//     .then(() => {
+//         console.log('then')
+//         console.log(pr)
+//     })
+//     .catch(() => {
+//         console.log(pr)
+//         console.warn('it is not resolve')
+//     })
+//     .finally(() => console.log('finally'))
+
+// console.log(3)
+
+const repo = {
+    save: (data: any) => {
+        try {
+            localStorage.setItem('newData', JSON.stringify(data))
+        } catch (error: any) {
+            return false
+        }
+        return true
+    },
+    saveAsync: (data: any) => {
+        const pr = new Promise((res, rej) => {
+            try {
+                localStorage.setItem('newData', JSON.stringify(data))
+                res()
+            } catch (error: any) {
+                return rej(error)
+            }
+
+        })
+        return pr
+    },
+    readAsync: () => {
+        return new Promise((res, rej) => {
+            const data = localStorage.getItem('newData')
+            res(data && JSON.parse(data))
+        })
+    }
+}
+
+repo.saveAsync({name: 'Miha', age: 32})
+    .then(response => console.log('save'))
+    .catch(error => console.warn('it doesn\'t save: ' + error))
+
+
+const run = async () => {
+    await repo.saveAsync({name: 'Miha', age: 32})
+    console.log('save')
+    console.log(await repo.readAsync());
+}
+run()
+
+
+const wait = (t: number) => new Promise((res, rej) => {
+    setInterval(() => {
+        res()
+    }, t)
+})
+
+const couter = async () => {
+    await wait(1000)
+    console.log(1)
+    await wait(2000)
+    console.log(2)
+    await wait(3000)
+    console.log(3)
+
+}
+couter()
+
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
 
 // just a plug
-export default ()=>{};
